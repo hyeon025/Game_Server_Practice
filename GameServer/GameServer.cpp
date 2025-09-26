9b03bb7e3a2f4d451ed3f2f6c1c8327d584d64d1
hyeon025 <kjh04255@naver.com>
2025-09-27 00:32:38 +0900

    Thread 명령어 디버그로 확인하기

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -12,17 +12,16 @@ int main()
 {
     std::thread t; //만들기만 하는 것
     
-    t = std::thread(HelloThread); //뒤늦게 쓰레드 구동이 되도록 만들 수 있음.
+    auto id = t.get_id(); //쓰레드 id가 0이 나온다. 
 
-    int32 count = t.hardware_concurrency(); // CPU 코어 개수 정보 추출(추출이 불가할 때는 0 리턴)
-    auto id = t.get_id(); //쓰레드마다 id
-    t.detach(); //std::thread 객체에서 실제 쓰레드를 분리(t를 이용해서 정보를 추출할 수 없어짐 - 거의 안씀)
-    t.joinable(); //연동된 쓰레드가 없는 걸 확인
+    t = std::thread(HelloThread); //이제야 id가 생겼다.
+
+    int32 count = t.hardware_concurrency(); //난 8개 나옴
+    auto id2 = t.get_id(); //id가 생긴 것을 볼 수 있다.
+
+    if(t.joinable()) //t가 join상태이면
+        t.join(); //t가 끝날 때까지 대기
 
     cout << "Hello Main" << endl;
-    
-
-
-    t.join(); //t가 끝날 때까지 대기
 
 }
