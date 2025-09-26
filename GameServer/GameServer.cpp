3893cdeca3a63e08031983ce098e4f52e7d8cad5
hyeon025 <kjh04255@naver.com>
2025-09-26 23:37:21 +0900

    쓰레드 생성 후 함수 실행

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,7 +2,19 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread> //윈도우,리눅스 등 다양한 환경에서 돌아가는 쓰레드 표준
+
+void HelloThread() {
+    cout << "Hello Thread" << endl;
+}
+
 int main()
 {
-    HelloWorld();
+    HelloThread(); //main 쓰레드가 함수 호출
+    std::thread t(HelloThread); //새로운 쓰레드가 생성되고 해당 함수를 실행 (main과 병렬로 실행)
+
+    cout << "Hello Main" << endl;
+
+    t.join(); //t가 끝날 때까지 대기
+
 }
