2349915956e9a96ad463fd473d1d8f90c2f18e6b
hyeon025 <kjh04255@naver.com>
2025-09-28 20:15:32 +0900

    LockGuard 함수로 자동 락 구현

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -10,17 +10,40 @@ vector<int32> v;
 //Mutual Exclusive (상호배타적)
 mutex m;
 
+//RAII (Resource Acquisition Is Initialization)
+template<typename T>
+class LockGuard
+{
+public:
+	LockGuard(T& m)
+	{
+		_mutex = &m;
+		_mutex->lock();
+	}
+
+	~LockGuard()
+	{
+		_mutex->unlock();
+	}
+
+private:
+	T* _mutex;
+};
+
 void Push()
 {
 	for (int32 i = 0; i < 10000; i++)
 	{
 		//자물쇠 잠그기
-		m.lock();
+		LockGuard<std::mutex> lockGuard(m); //알아서 락 하고 풀어준다.
+		//m.lock();
+		//m.lock();
 
 		v.push_back(i);
 
 		//자물쇠 풀기
-		m.unlock();
+		//m.unlock();
+		//m.unlock();
 	}
 }
 
