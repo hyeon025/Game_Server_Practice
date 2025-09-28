5e9df5c82d3b22b8c4887d46586b5e927f7e1afa
hyeon025 <kjh04255@naver.com>
2025-09-28 19:45:32 +0900

    vector에서 발생하는 double free 문제

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,7 +4,30 @@
 #include <thread>
 #include <atomic>
 
+// [1][2][3]
+// 메모리가 꽉차면
+// [1][2][3][4][5][6]
+// 더 큰 메모리 할당해서 이전 메모리 복사하고 삭제
+
+//두개의 쓰레드가 둘 다 delete를 해서 double free 문제 발생.
+
+vector<int32> v;
+
+void Push()
+{
+	for (int32 i = 0; i < 10000; i++)
+	{
+		v.push_back(i);
+	}
+}
+
 int main()
 {
-	
+	std::thread t1(Push);
+	std::thread t2(Push);
+
+	t1.join();
+	t2.join();
+
+	cout << v.size() << endl;
 }
