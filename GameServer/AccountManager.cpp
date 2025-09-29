071d425965973f1440e33f209b5102d77ea63fb9
hyeon025 <kjh04255@naver.com>
2025-09-30 02:01:09 +0900

    두개의 lock을 동시에 가능한 경우

---
+++ GameServer/AccountManager.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "AccountManager.h"
+#include "UserManager.h"
+
+void AccountManager::ProcessLogin()
+{
+	//accountLock
+	lock_guard<mutex> guard(_mutex);
+
+
+	//userLock
+	User* user = UserManager::Instance()->GerUser(100);
+
+}
---
+++ GameServer/UserManager.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "UserManager.h"
+
+void UserManager::ProcessSave()
+{
+	lock_guard<mutex> guard(_mutex);
+
+}
