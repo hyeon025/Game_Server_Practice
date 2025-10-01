934565a9af069c449c8177d168d88b92662c389a
hyeon025 <kjh04255@naver.com>
2025-10-02 02:29:43 +0900

    데드락 상황 발생

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,8 +4,34 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include "AccountManager.h"
+#include "UserManager.h"
+
+void Func1()
+{
+	for (int32 i = 0; i < 100; i++)
+	{
+		UserManager::Instance()->ProcessSave();
+	}
+}
+
+void Func2()
+{
+	for (int32 i = 0; i < 100; i++)
+	{
+		AccountManager::Instance()->ProcessLogin();
+	}
+}
 
 int main()
 {
-	
+	std::thread t1(Func1);
+	std::thread t2(Func2); 
+	//데드락 상황 발생 (서로 락 걸려서 둘 다 기다리는 상황)
+
+
+	t1.join();
+	t2.join();
+
+	cout << "Jobs Done" << endl;
 }
--- GameServer/UserManager.cpp
+++ GameServer/UserManager.cpp
@@ -1,8 +1,12 @@
 #include "pch.h"
 #include "UserManager.h"
+#include "AccountManager.h"
 
 void UserManager::ProcessSave()
 {
+	//userLock
 	lock_guard<mutex> guard(_mutex);
 
+	//accountLock
+	Account* account = AccountManager::Instance()->GetAccount(100);
 }
