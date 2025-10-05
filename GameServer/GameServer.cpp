f5a9e3d94eaea0766614ba9864c2959f961dfa4a
hyeon025 <kjh04255@naver.com>
2025-10-05 15:15:10 +0900

    기본적인 lock guard 구현

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,8 +5,34 @@
 #include <atomic>
 #include <mutex>
 
+int32 sum = 0;
+mutex m;
+
+void Add()
+{
+	for (int32 i = 0; i < 10'000; i++)
+	{
+		lock_guard<mutex> guard(m);
+		sum++;
+	}
+}
+
+void Sub()
+{
+	for (int32 i = 0; i < 10'000; i++)
+	{
+		lock_guard<mutex> guard(m);
+		sum--;
+	}
+}
 
 int main()
 {
-	
+	thread t1(Add);
+	thread t2(Sub);
+
+	t1.join();
+	t2.join();
+
+	cout << sum << endl;
 }
