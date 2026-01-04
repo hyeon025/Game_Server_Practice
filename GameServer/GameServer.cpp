0a838c710a71c1d0e42ac0eca707d58dad7267e2
hyeon025 <kjh04255@naver.com>
2026-01-04 15:09:48 +0900

    생산자 소비자 (소비자가 while문으로 계속 체크해서 cpu 점유율 높음)

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,9 +4,44 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <windows.h>
 
+mutex m;
+queue<int32> q;
+
+void Producer() 
+{
+	while (true)
+	{
+		{
+			unique_lock<mutex> lock(m);
+			q.push(100);
+		}
+		this_thread::sleep_for(chrono::milliseconds(100));
+	}
+}
+
+void Consumer()
+{
+	while (true)
+	{
+		{
+			unique_lock<mutex> lock(m);
+			if (q.empty() == false)
+			{
+				int32 data = q.front();
+				q.pop();
+				cout << data << endl;
+			}
+		}
+	}
+}
 
 int main()
 {
-	
+	thread t1(Producer);
+	thread t2(Consumer);
+
+	t1.join();
+	t2.join();
 }
