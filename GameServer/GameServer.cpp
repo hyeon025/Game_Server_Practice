fa32b1aa668e2998339ac24a59498859a2dc0d61
hyeon025 <kjh04255@naver.com>
2026-01-04 15:27:33 +0900

    커널 오브젝트로 보완(Signal 사용)

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,6 +8,7 @@
 
 mutex m;
 queue<int32> q;
+HANDLE handle;
 
 void Producer() 
 {
@@ -17,7 +18,9 @@ void Producer()
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
-		this_thread::sleep_for(chrono::milliseconds(100));
+
+		::SetEvent(handle); //Signal 상태로 변경
+		this_thread::sleep_for(chrono::milliseconds(1000));
 	}
 }
 
@@ -25,23 +28,33 @@ void Consumer()
 {
 	while (true)
 	{
+		::WaitForSingleObject(handle, INFINITE); //Signal 상태가 될때까지 대기
+
+		unique_lock<mutex> lock(m);
+		if (q.empty() == false)
 		{
-			unique_lock<mutex> lock(m);
-			if (q.empty() == false)
-			{
-				int32 data = q.front();
-				q.pop();
-				cout << data << endl;
-			}
+			int32 data = q.front();
+			q.pop();
+			cout << data << endl;
 		}
 	}
 }
 
 int main()
 {
+
+	//커널 오브젝트(커널에서 관리하는 객체)
+	//Usage count (참조 카운트)
+	//Signal / Non-signal
+	//Auto / Manual
+	handle = ::CreateEvent(NULL, FALSE, FALSE, NULL); //보안속성, 메뉴얼리셋, 초기상태, 이름
+	
 	thread t1(Producer);
 	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
+
+	::CloseHandle(handle);
+
 }
