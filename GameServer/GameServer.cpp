23c68e1d15f5a0e294dd7afce874444ca95b72f9
hyeon025 <kjh04255@naver.com>
2026-01-31 18:06:05 +0900

    동기 방식

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,3 +4,26 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <future>
+
+int64 Calculate()
+{
+	int64 sum = 0;
+
+	for (int32 i = 0; i < 100'000; i++)
+	{
+		sum += i;
+	}
+
+	return sum;
+}
+
+int main()
+{
+	//동기 방식의 실행 (synchronous)
+	int64 sum = Calculate();
+	cout << sum << endl;
+
+
+
+}
